0c01207f2f0b3a1c0fa0de2ae9ea7f16b8c3636f
David Smith <dave@avaragado.org>
2017-03-16 12:46:12 +0000

    Wire up pt-BR locale and messages statically

--- src/index.js
+++ src/index.js
@@ -7,14 +7,19 @@ import { IntlProvider, addLocaleData } from 'react-intl';
 
 // add locale data statically: could be done dynamically, eg see react-intl-loader
 import 'intl/locale-data/jsonp/en-GB';
+import 'intl/locale-data/jsonp/pt-BR';
 import en from 'react-intl/locale-data/en';
+import pt from 'react-intl/locale-data/pt';
+
+import messages from './locale/pt-BR.json';
 
 import App from './components/App';
 
 addLocaleData(en);
+addLocaleData(pt);
 
 ReactDOM.render(
-    <IntlProvider locale="en-GB">
+    <IntlProvider key="pt-BR" locale="pt-BR" messages={messages}>
         <App/>
     </IntlProvider>,
     document.getElementById('root')
